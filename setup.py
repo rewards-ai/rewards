381e75eaca0b6a3a855629e8af2f30ff03d7ca7c
Pratyush-exe <pratyush21225@gmail.com>
2023-04-04 18:45:16 +0530

    changing version

--- setup.py
+++ setup.py
@@ -1,43 +1,43 @@
-import os
-import io
-import setuptools
+# import os
+# import io
+# import setuptools
     
-def read(*paths, **kwargs):
-    """Read the contents of a text file safely.
-    >>> read("rewards", "VERSION")
-    '0.1.0'
-    >>> read("README.md")
-    ...
-    """
+# def read(*paths, **kwargs):
+#     """Read the contents of a text file safely.
+#     >>> read("rewards", "VERSION")
+#     '0.1.0'
+#     >>> read("README.md")
+#     ...
+#     """
 
-    content = ""
-    with io.open(
-        os.path.join(os.path.dirname(__file__), *paths),
-        encoding=kwargs.get("encoding", "utf8"),
-    ) as open_file:
-        content = open_file.read().strip()
-    return content
+#     content = ""
+#     with io.open(
+#         os.path.join(os.path.dirname(__file__), *paths),
+#         encoding=kwargs.get("encoding", "utf8"),
+#     ) as open_file:
+#         content = open_file.read().strip()
+#     return content
 
-def read_requirements(path):
-    return [
-        line.strip()
-        for line in read(path).split("\n")
-        if not line.startswith(('"', "#", "-", "git+"))
-    ]
+# def read_requirements(path):
+#     return [
+#         line.strip()
+#         for line in read(path).split("\n")
+#         if not line.startswith(('"', "#", "-", "git+"))
+#     ]
 
-setuptools.setup(
-    name="rewardsAI",
-    version="0.0.0.7",
-    author="rewards-ai",
-    description="Low-code RL agent training and evaluating platform.",
-    long_description=read("README.md"),
-    long_description_content_type="text/markdown",
-    url="https://github.com/rewards-ai/rewards-sdk",
-    python_requires='>=3.9',
-    packages=setuptools.find_packages(exclude=["tests", ".github"]),
-    include_package_data=True,
-    package_data={
-        'rewardsAI': ['assets/CarRace/*.png'],
-    },
-    install_requires=read_requirements("requirements.txt")
-)
+# setuptools.setup(
+#     name="rewardsAI",
+#     version="0.0.0.7",
+#     author="rewards-ai",
+#     description="Low-code RL agent training and evaluating platform.",
+#     long_description=read("README.md"),
+#     long_description_content_type="text/markdown",
+#     url="https://github.com/rewards-ai/rewards-sdk",
+#     python_requires='>=3.9',
+#     packages=setuptools.find_packages(exclude=["tests", ".github"]),
+#     include_package_data=True,
+#     package_data={
+#         'rewardsAI': ['assets/CarRace/*.png'],
+#     },
+#     install_requires=read_requirements("requirements.txt")
+# )
