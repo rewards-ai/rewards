74b137760d76177ff91c4d65185910e20b5d2407
Pratyush-exe <pratyush21225@gmail.com>
2023-09-19 19:34:06 +0530

    FEAT:Updated examples/car/train.py with procedurally generated tracks

--- examples/car/train.py
+++ examples/car/train.py
@@ -1,14 +1,24 @@
-from rewards.environments.pygame.car_racer import Car, CarRacer, CarRacerConfig
 from rewards.models import LinearQNet
 from rewards.algorithms import DQN
+from rewards.environments.pygame.car_racer import Track, Car, CarRacer, CarRacerConfig
 import pygame
 
 pygame.init()
 
-# Initialize cars, models and trainers
-cars = [Car(id="titu", radar_nums=5, show_radar=False)]
+# Constants
+WIDTH, HEIGHT = 800, 700
+track = Track(size=(WIDTH, HEIGHT), generated=True, seed=-1)
+
+cars = [
+    Car(id="agent-1", radar_nums=5, show_radar=True, angle=track.direction, center=track.start_point),
+    Car(id="agent-2", radar_nums=5, show_radar=True, angle=track.direction, center=track.start_point)
+]
+
 models = [LinearQNet([[cars[i].radar_nums, 64], [64, 3]]) for i in range(len(cars))]
-trainers = [DQN(models[i], epsilon=0.2, optimzer="adam") for i in range(len(cars))]
+trainers = [DQN(models[i], epsilon=0.2, optimzer="adam", epsilon_decay_rate=0.0001) for i in range(len(cars))]
+
+# Create a Pygame window
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 # define a reward function
 def reward_function(params):
@@ -17,7 +27,12 @@ def reward_function(params):
     return 0
 
 # create environment (CarRacer) with CarRacerConfig
-racer_config = CarRacerConfig(agents=cars, reward_function=reward_function)
+racer_config = CarRacerConfig(
+    agents=cars, 
+    reward_function=reward_function,
+    track=track,
+    FPS=50
+)
 racer = CarRacer(racer_config)
 
 # Initialize trackers
@@ -26,9 +41,8 @@ total_scores = [0] * len(cars)
 records = [0] * len(cars)
 dones = [False] * len(cars)
 n_games = [0] * len(cars)
-        
-        
-# define custom game loop
+
+# Main game loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -59,8 +73,9 @@ while True:
             
             # Logging
             import os
-            os.system('cls')
+            # os.system('cls')
+            print(track.direction)
             for j in range(len(cars)):
                 print(cars[j].id, records[j], ":", "n_games:", n_games[j])
-                print(trainers[j].epsilon)
+                print("epsilon", trainers[j].epsilon)
                 print("\n")
